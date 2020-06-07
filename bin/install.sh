be97da12b85095c759ab230617b777cfd3be9a67
Yuma Ishikawa <yuma.fuu05@gmail.com>
2020-06-08 00:20:08 +0900

    [fix] install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -3,4 +3,12 @@ echo "install my nvim conf"
 path="$HOME/.config/nvim"
 mkdir -p $path && cd $NVIM
 
-git clone https://github.com/YumaFuu/nvim.git
+if [ $(uname) = "Darwin" ]; then
+  brew install nvim
+elif [ $(uname) = "Linux" ]; then
+  apt-get update && apt-get install nvim
+else
+  exit 1
+fi
+
+git clone https://github.com/YumaFuu/nvim.git $path
