39e5fd86028e6c8e4999b143a1256645d3367a87
Yuma Ishikawa <yuma.fuu05@gmail.com>
2020-06-08 09:05:08 +0900

    updated at 2020/06/08 09:05:08

--- bin/update.sh
+++ bin/update.sh
@@ -1,13 +1,10 @@
 #!bash
+path="/Users/`whoami`/.config/nvim"
 
-if [ $NVIM = "" ] ; then
-  export $NVIM="/Users/`whoami`/.config/nvim"
-fi
-
-mkdir -p $NVIM
-cd $NVIM
+cd $path
 now=$(date "+%Y/%m/%d %H:%M:%S")
 
 git add -A
 git commit -m"updated at ${now}"
 git push origin master
+cd -
